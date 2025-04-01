46e0d0b84f8e8932f54fd77c6ad64005b28025f8
Robbie <hannibal.chiu@gmail.com>
2025-04-01 11:56:43 +0800

    系統核銷通知參考區塊

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -10,16 +10,24 @@ import { RevenueStreamWidget } from './components/revenuestreamwidget';
     imports: [StatsWidget, RecentSalesWidget, BestSellingWidget, RevenueStreamWidget, NotificationsWidget],
     template: `
         <div class="grid grid-cols-12 gap-8">
-            <!--<app-stats-widget class="contents" />-->
-            <div class="col-span-12 xl:col-span-6">
+            <div class="col-span-12 xl:col-span-12">
                 <app-recent-sales-widget />
-                <app-best-selling-widget />
             </div>
-            <div class="col-span-12 xl:col-span-6">
-                <app-revenue-stream-widget />
-                <app-notifications-widget />
-            </div>
+            <app-stats-widget class="contents" />
         </div>
     `
+    // template: `
+    //     <div class="grid grid-cols-12 gap-8">
+    //         <app-stats-widget class="contents" />
+    //         <div class="col-span-12 xl:col-span-6">
+    //             <app-recent-sales-widget />
+    //             <app-best-selling-widget />
+    //         </div>
+    //         <div class="col-span-12 xl:col-span-6">
+    //             <app-revenue-stream-widget />
+    //             <app-notifications-widget />
+    //         </div>
+    //     </div>
+    // `
 })
 export class Dashboard {}
